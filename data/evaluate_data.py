f80e324446b999a34893d32ec7763626ac24458f
mwiszenko <wiszenkom@gmail.com>
2021-04-11 14:41:08 +0200

    Add calculation of daily interactions, valid/reproducible events and matrix density

--- data/evaluate_data.py
+++ data/evaluate_data.py
@@ -1,51 +1,71 @@
 import json
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
 
 def load_jsonl(path):
-    data=[]
+    data = []
     with open(path, 'r', encoding='utf-8') as reader:
         for line in reader:
             data.append(json.loads(line))
     return data
-    
+
+
+def calc_percentage(part, whole, digits):
+    percentage = 100 * float(part) / float(whole)
+    rounded_percentage = round(percentage, digits)
+    return str(rounded_percentage) + '%'
+
+
+def make_percentage(number, digits):
+    percentage = 100 * number
+    rounded_percentage = round(percentage, digits)
+    return str(rounded_percentage) + '%'
+
 
 if __name__ == '__main__':
-    u_cities = list()
-    users = load_jsonl("users.jsonl")
-    for user in users:
-        u_cities.append(user['city'])
-    print("Users - cities: ")
-    print('\t', "all: ", len(u_cities), ", unique: ", len(set(u_cities)), ", missing: ", u_cities.count(None))
-    
-    p_prices = list()
-    p_categories = list()
-    products = load_jsonl("products.jsonl")
-    for product in products:
-        p_categories.append(product['category_path'])
-        p_prices.append(product['price'])
-    print("Products - categories: ")
-    print('\t', "all: ", len(p_categories), ", unique: ", len(set(p_categories)), ", missing: ", p_categories.count(None))
-    print("Products - prices: ")
-    print('\t', "all: ", len(p_prices), ", unique: ", len(set(p_prices)), ", missing: ", p_prices.count(None), ", min: ", min(p_prices), ", max: ", max(p_prices))
+    valid_events, all_events, valid_or_repr_events = 0, 0, 0
+    purchases, views = 0, 0
+    user_ids = set()
+    users_data = load_jsonl("users.jsonl")
+    for user in users_data:
+        user_ids.add(user['user_id'])
 
-    s_user_ids = list()
-    s_product_ids = list()
-    s_event_types = list()
-    s_offered_discounts = list()
-    s_purchase_ids = list()
-    sessions = load_jsonl("sessions.jsonl")
-    for session in sessions:
-        s_user_ids.append(session['user_id'])
-        s_product_ids.append(session['product_id'])
-        s_event_types.append(session['event_type'])
-        s_offered_discounts.append(session['offered_discount'])
-        s_purchase_ids.append(session['purchase_id'])
-    print("Sessions - user ids: ")
-    print('\t', "all: ", len(s_user_ids), ", unique: ", len(set(s_user_ids)), ", missing: ", s_user_ids.count(None))
-    print("Sessions - product ids: ")
-    print('\t', "all: ", len(s_product_ids), ", unique: ", len(set(s_product_ids)), ", missing: ", s_product_ids.count(None))
-    print("Sessions - event types: ")
-    print('\t', "all: ", len(s_event_types), ", unique: ", len(set(s_event_types)), ", missing: ", s_event_types.count(None))
-    print("Sessions - offered discounts: ")
-    print('\t', "all: ", len(s_offered_discounts), ", unique: ", len(set(s_offered_discounts)), ", missing: ", s_offered_discounts.count(None), ", min: ", min(s_offered_discounts), ", max: ", max(s_offered_discounts))
-    print("Sessions - purchase ids: ")
-    print('\t', "all: ", len(s_purchase_ids), ", unique: ", len(set(s_purchase_ids)), ", missing: ", s_purchase_ids.count(None))
+    product_ids = set()
+    products_data = load_jsonl("products.jsonl")
+    for product in products_data:
+        product_ids.add(product['product_id'])
+
+    matrix = np.zeros([len(user_ids), len(product_ids)])
+    df = pd.DataFrame(matrix, columns=product_ids, index=user_ids)
+    sessions_dict = dict()
+    dates_set = set()
+    events = list()
+    events_data = load_jsonl("sessions.jsonl")
+    for event in events_data:
+        if event['user_id'] is not None:
+            sessions_dict[event['session_id']] = event['user_id']
+            if event['product_id'] is not None:
+                valid_events += 1
+        all_events += 1
+    for event in events_data:
+        if event['product_id'] is not None:
+            if event['session_id'] in sessions_dict.keys():
+                df[event['product_id']][sessions_dict[event['session_id']]] = 1
+                valid_or_repr_events += 1
+                dates_set.add(datetime.strptime(event['timestamp'], '%Y-%m-%dT%H:%M:%S').date())
+                if event['event_type'] == 'BUY_PRODUCT':
+                    purchases += 1
+                elif event['event_type'] == 'VIEW_PRODUCT':
+                    views += 1
+
+    print('Users:', len(user_ids))
+    print('Products:', len(product_ids))
+    print('All events:', all_events)
+    print('Initially valid events:', valid_events, calc_percentage(valid_events, all_events, 2))
+    print('Valid or reproducible events:', valid_or_repr_events, calc_percentage(valid_or_repr_events, all_events, 2))
+    print('Density', make_percentage(np.average(df.to_numpy()), 2))
+    print('Daily sessions:', round(len(sessions_dict) / len(dates_set)))
+    print('Daily views:', round(views / len(dates_set)))
+    print('Daily purchases:', round(purchases / len(dates_set)))
