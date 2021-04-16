3f151344b84c24a607e29ee48a1f958736306525
Łukasz <maniak656@gmail.com>
2021-04-16 15:51:16 +0200

    New data

---
+++ data/data_analyser.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import collections as col
+
+prices = pd.read_json("products.jsonl", lines=True)
+#missing = pd.DataFrame({'total_missing': sessions.isnull().sum(), 'percent': (sessions.isnull().sum() / 84296) * 100})
+#print(missing)
+column = 'price'
+ceny = []
+negativeVal = 0
+enormousVal = 0
+for i in prices[column]:
+    ceny.append(i)
+ceny.sort()
+numCeny = len(ceny)
+print(prices[column].describe())
+for i in ceny:
+    print(i)
+    if float(i) <= 0: negativeVal += 1
+    elif float(i) >= 10000: enormousVal += 1
+
+correctPrices = negativeVal + enormousVal
+print('All products: ', numCeny)
+print('Correct prices: ', numCeny - negativeVal - enormousVal)
+print('% of correct prices: ', (numCeny - correctPrices) / numCeny * 100)
+
+plt.figure(figsize=(5,5))
+prices[column].plot.box()
+plt.show()
+
+plt.figure(figsize=(10,5))
+plt.hist(prices[column][prices[column].notnull()], 500, facecolor='blue')
+plt.show()
