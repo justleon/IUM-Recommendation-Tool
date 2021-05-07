687d0de3777b2de3e4b1cc1a190dcd308dd98c59
mwiszenko <wiszenkom@gmail.com>
2021-05-07 21:42:44 +0200

    Change histogram scales to logarytmic

--- data/data_analyser.py
+++ data/data_analyser.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import collections as col
+import numpy as np
 
 prices = pd.read_json("products.jsonl", lines=True)
 #missing = pd.DataFrame({'total_missing': sessions.isnull().sum(), 'percent': (sessions.isnull().sum() / 84296) * 100})
@@ -24,10 +25,12 @@ print('All products: ', numCeny)
 print('Correct prices: ', numCeny - negativeVal - enormousVal)
 print('% of correct prices: ', (numCeny - correctPrices) / numCeny * 100)
 
-plt.figure(figsize=(5,5))
+plt.figure(figsize=(5, 5))
 prices[column].plot.box()
+plt.gca().set_yscale("log")
 plt.show()
 
-plt.figure(figsize=(10,5))
+plt.figure(figsize=(10, 5))
 plt.hist(prices[column][prices[column].notnull()], 500, facecolor='blue')
+plt.gca().set_xscale("log")
 plt.show()
