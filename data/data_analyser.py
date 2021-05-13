0e5ca61b5cd581503cff836892858b5539ef076a
Łukasz <maniak656@gmail.com>
2021-05-13 02:09:17 +0200

    user_id distribution in sessions

--- data/data_analyser.py
+++ data/data_analyser.py
@@ -3,18 +3,17 @@ import matplotlib.pyplot as plt
 import collections as col
 import numpy as np
 
-prices = pd.read_json("products.jsonl", lines=True)
-#missing = pd.DataFrame({'total_missing': sessions.isnull().sum(), 'percent': (sessions.isnull().sum() / 84296) * 100})
-#print(missing)
+products = pd.read_json("products.jsonl", lines=True)
+sessions = pd.read_json("sessions.jsonl", lines=True)
 column = 'price'
 ceny = []
 negativeVal = 0
 enormousVal = 0
-for i in prices[column]:
+for i in products['price']:
     ceny.append(i)
 ceny.sort()
 numCeny = len(ceny)
-print(prices[column].describe())
+print(products['price'].describe())
 for i in ceny:
     print(i)
     if float(i) <= 0: negativeVal += 1
@@ -26,11 +25,19 @@ print('Correct prices: ', numCeny - negativeVal - enormousVal)
 print('% of correct prices: ', (numCeny - correctPrices) / numCeny * 100)
 
 plt.figure(figsize=(5, 5))
-prices[column].plot.box()
+products['price'].plot.box()
 plt.gca().set_yscale("log")
 plt.show()
 
 plt.figure(figsize=(10, 5))
-plt.hist(prices[column][prices[column].notnull()], 500, facecolor='blue')
+plt.hist(products['price'], 500, facecolor='blue')
 plt.gca().set_xscale("log")
+plt.show()
+
+plt.figure(figsize=(5, 5))
+plt.hist(sessions['event_type'], facecolor='blue')
+plt.show()
+
+plt.figure(figsize=(5, 5))
+plt.hist(sessions['user_id'], 200, facecolor='blue')
 plt.show()
