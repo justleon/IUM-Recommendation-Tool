8c75e472001d1de38f03eecff1f0660e737782ac
mwiszenko <wiszenkom@gmail.com>
2021-05-23 14:24:03 +0200

    Move popularity calculation to init

--- recommender/models.py
+++ recommender/models.py
@@ -1,10 +1,10 @@
 class PopularityBasedRecommender:
     def __init__(self, data_handler):
         self.data_handler = data_handler
+        self.product_popularity = self.data_handler.data.groupby('product_id').size().sort_values(ascending=False) \
+            .reset_index(name='popularity')
 
     def predict(self, user_id: int):
-        if user_id not in set(self.data_handler.users['user_id']):
-            return None
-        product_popularity = self.data_handler.data.groupby('product_id').size().sort_values(ascending=False) \
-            .reset_index(name='popularity')
-        return product_popularity['product_id'].to_list()
+        if user_id in set(self.data_handler.users['user_id']):
+            return self.product_popularity['product_id'].to_list()
+        return None
