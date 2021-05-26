0df8bf5a340b87114e8235de4069f03927306c4d
Łukasz <maniak656@gmail.com>
2021-05-26 15:10:50 +0200

    Model Evaluation

--- recommender/data_handler.py
+++ recommender/data_handler.py
@@ -3,8 +3,11 @@ from sklearn.model_selection import train_test_split
 import math
 from utils import load_jsonl_pd
 
+SEED = 1
 MIN_INTERACTIONS = 5
 TEST_SIZE = 0.2
+VIEW_PRODUCT_STRENGTH = 1
+BUY_PRODUCT_STRENGTH = 3
 
 
 def merge(
@@ -14,13 +17,22 @@ def merge(
     merged = pd.merge(merged, products, on='product_id')
     return merged
 
+
 def smooth_preference(x):
     return math.log(1 + x, 2)
+
+
+def get_items_interacted(user_id: int, data_set):
+    interacted_items = data_set.loc[user_id]['product_id']
+    return set(interacted_items if type(interacted_items) == pd.Series else [interacted_items])
+
 
 class DataHandler:
     def __init__(self):
         sessions = load_jsonl_pd("data/sessions.jsonl")
-        sessions = sessions.replace({'event_type': {'VIEW_PRODUCT': 1, 'BUY_PRODUCT': 3}})
+        sessions = sessions.replace({'event_type':
+                                         {'VIEW_PRODUCT': VIEW_PRODUCT_STRENGTH,
+                                          'BUY_PRODUCT': BUY_PRODUCT_STRENGTH}})
         self.products = load_jsonl_pd("data/products.jsonl")
         self.users = load_jsonl_pd("data/users.jsonl")
         self.sessions = merge(sessions, self.products, self.users)
@@ -30,21 +42,20 @@ class DataHandler:
         users_enough_interactions = user_interactions_num[user_interactions_num >= MIN_INTERACTIONS] \
             .reset_index(['user_id'])
         print("users: " + str(len(user_interactions_num)))
+
         interactions = sessions.merge(users_enough_interactions, how='right', left_on='user_id', right_on='user_id')
         self.interactions = interactions.groupby(['user_id', 'product_id'])['event_type'].sum() \
             .apply(smooth_preference).reset_index()
         print("total unique interactions: " + str(len(interactions)))
+
         interactions_train, interactions_test = train_test_split(interactions, stratify=interactions['user_id'],
-                                                                 test_size=TEST_SIZE, random_state=1)
+                                                                 test_size=TEST_SIZE, random_state=SEED)
         print("train set: " + str(len(interactions_train)))
         print("test set: " + str(len(interactions_test)))
 
+        #to speed up the search process during evaluation we index the sets
         self.interactions_indexed = interactions.set_index('user_id')
         self.interactions_train_indexed = interactions_train.set_index('user_id')
         self.interactions_test_indexed = interactions_test.set_index('user_id')
         self.item_popularity = self.interactions.groupby('product_id')['event_type'].sum() \
             .sort_values(ascending=False).reset_index()
-
-    def get_items_interacted(self, user_id: int, data_set):
-        interacted_items = data_set.loc[user_id]['product_id']
-        return set(interacted_items if type(interacted_items) == pd.Series else [interacted_items])
---
+++ recommender/model_evaluation.py
@@ -0,0 +1,84 @@
+import random
+import pandas as pd
+from data_handler import get_items_interacted
+
+SEED = 1
+EVAL_RANDOM_SAMPLE_NON_INTERACTED_ITEMS = 100
+
+
+class ModelEvaluator:
+    def __init__(self, data_handler):
+        self.data_handler = data_handler
+
+    def get_not_interacted_items_sample(self, user_id: int, sample_size, seed=SEED):
+        random.seed(seed)
+
+        interacted_items = get_items_interacted(user_id, self.data_handler.interactions_indexed)
+        all_items = set(self.data_handler.products['product_id'])
+        non_interacted = all_items - interacted_items
+
+        non_interacted_sample = random.sample(non_interacted, sample_size)
+        return set(non_interacted_sample)
+
+    def if_hit_top_n(self, product_id, recommended_items, top_n):
+        try:
+            index = next(i for i, c in enumerate(recommended_items) if c == product_id)
+        except:
+            index = -1
+        hit = int(index in range(0, top_n))
+        return hit, index
+
+    def evaluate_model_for_user(self, model, user_id):
+        interacted_vals_test = self.data_handler.interactions_test.loc['user_id']
+        if type(interacted_vals_test['product_id']) == pd.Series:
+            person_interacted_items_test = set(interacted_vals_test['product_id'])
+        else:
+            person_interacted_items_test = set([int(interacted_vals_test['product_id'])])
+        interacted_items_count_test = len(person_interacted_items_test)
+
+        user_recommendations = model.predict(user_id)
+        hits_at_5_num = 0
+        hits_at_10_num = 0
+
+        for product_id in person_interacted_items_test:
+            non_interacted_sample = self.get_not_interacted_items_sample(user_id,
+                                                 EVAL_RANDOM_SAMPLE_NON_INTERACTED_ITEMS,
+                                                 product_id % (2 ** 32))
+            items_to_filter_recommendations = non_interacted_sample.union(set([product_id]))
+            valid_recommendations_temp = user_recommendations[
+                user_recommendations['product_id'].isin(items_to_filter_recommendations)]
+            valid_recommendations = valid_recommendations_temp['product_id'].values
+            hit_at_5, index_at_5 = self.if_hit_top_n(product_id, valid_recommendations, 5)
+            hits_at_5_num += hit_at_5
+            hit_at_10, index_at_10 = self.if_hit_top_n(product_id, valid_recommendations, 10)
+            hits_at_10_num += hit_at_10
+
+        rate_at_5 = hits_at_5_num / float(interacted_items_count_test)
+        rate_at_10 = hits_at_10_num / float(interacted_items_count_test)
+
+        user_metrics = {
+            'hits@5_count': hits_at_5_num,
+            'hits@10_count': hits_at_10_num,
+            'interacted_count': interacted_items_count_test,
+            'recall@5': rate_at_5,
+            'recall@10': rate_at_10
+        }
+        return user_metrics
+
+    def evaluate(self, model):
+        users_metrics = []
+        for idx, user_id in enumerate(list(self.data_handler.interactions_test_indexed.index.unique().values)):
+            user_metrics = self.evaluate_model_for_user(model, user_id)
+            user_metrics['_user_id'] = user_id
+            users_metrics.append(user_metrics)
+        print('processed ' + str(idx) + ' users')
+
+        detailed_results = pd.DataFrame(users_metrics).sort_values('interacted_count', ascending=False)
+        global_rate_at_5 = detailed_results['hitrs@5_count'].sum() / float(detailed_results['interacted_count'].sum())
+        global_rate_at_10 = detailed_results['hitrs@10_count'].sum() / float(detailed_results['interacted_count'].sum())
+
+        global_metrics = {
+            'rate@5': global_rate_at_5,
+            'rate@10': global_rate_at_10
+        }
+        return global_metrics
