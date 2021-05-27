14a8188b6bf317f7c5426e5f890cfc97f897c58a
mwiszenko <wiszenkom@gmail.com>
2021-05-27 17:35:42 +0200

    Change module name

--- recommender/test_models.py
+++ recommender/test_models.py
@@ -1,15 +1,14 @@
 from models import PopularityBasedRecommender, ContentBasedRecommender
 from data_handler import DataHandler
+from model_evaluator import ModelEvaluator
 
 if __name__ == "__main__":
     dh = DataHandler()
     popularity_model = PopularityBasedRecommender(dh)
     content_based_model = ContentBasedRecommender(dh)
-
-    predict_popularity = popularity_model.predict(102)
-    predict_content = content_based_model.predict(102)
+    model_evaluator = ModelEvaluator(dh)
 
     print("---POPULARITY BASED MODEL---")
-    print(predict_popularity)
+    print(model_evaluator.evaluate(popularity_model))
     print("\n---CONTENT BASED MODEL---")
-    print(predict_content)
+    print(model_evaluator.evaluate(content_based_model))
