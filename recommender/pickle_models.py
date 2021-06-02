bce41871809b12e41d0c8640280f1a32e8e77859
mwiszenko <wiszenkom@gmail.com>
2021-06-03 01:40:52 +0200

    Fix paths for pickled models

--- recommender/pickle_models.py
+++ recommender/pickle_models.py
@@ -7,8 +7,8 @@ from models import ContentBasedRecommender, PopularityBasedRecommender
 
 
 def pickle_models():
-    popularity_path = 'models\\popularity_model.pkl'
-    content_based_path = "models\\content_based_model.pkl"
+    popularity_path = 'models/popularity_model.pkl'
+    content_based_path = "models/content_based_model.pkl"
     dh = DataHandler()
     popularity_model = PopularityBasedRecommender(dh)
     content_based_model = ContentBasedRecommender(dh)
@@ -25,11 +25,12 @@ def pickle_models():
 
     content_based_model.train(best_params[0], best_params[1], best_params[2])
 
-    with open(f'{ROOT}\\{popularity_path}', 'wb') as popularity_model_file:
+    with open(f'{ROOT}/{popularity_path}', 'wb') as popularity_model_file:
         pickle.dump(popularity_model, popularity_model_file)
     popularity_model_file.close()
 
-    with open(f'{ROOT}\\{content_based_path}', 'wb') as content_based_model_file:
+    print(ROOT)
+    with open(f'{ROOT}/{content_based_path}', 'wb') as content_based_model_file:
         pickle.dump(content_based_model, content_based_model_file)
     content_based_model_file.close()
 
