a0cd76356a943b2acf11749bdf57c37456d0dc2d
mwiszenko <wiszenkom@gmail.com>
2021-06-03 01:01:50 +0200

    Fix data set sizes

--- recommender/config.py
+++ recommender/config.py
@@ -2,9 +2,9 @@ import os
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
 SEED = 1
-TRAIN_SIZE = 0.2
 TEST_SIZE = 0.2
-VAL_SIZE = 0.6
+VAL_SIZE = 0.2
+TRAIN_SIZE = 1 - TEST_SIZE - VAL_SIZE
 VIEW_PRODUCT_STRENGTH = 1
 BUY_PRODUCT_STRENGTH = 3
 PARAMETER_LIST = [(0, 0.5, (1, 1)), (0, 0.5, (1, 2)), (0, 0.5, (1, 3))]
--- recommender/test_models.py
+++ recommender/test_models.py
@@ -12,17 +12,16 @@ if __name__ == "__main__":
     print("---POPULARITY BASED MODEL---")
     print(model_evaluator.evaluate(popularity_model, dh.interactions_test_indexed))
 
-    # curr_max = 0
-    # best_params = None
-    # for params in PARAMETER_LIST:
-    #     content_based_model.train(params[0], params[1], params[2])
-    #     result = model_evaluator.evaluate(content_based_model, dh.interactions_val_indexed)
-    #     if (result['rate@5'] + result['rate@10']) > curr_max:
-    #         curr_max = result['rate@5'] + result['rate@10']
-    #         best_params = params
+    curr_max = 0
+    best_params = None
+    for params in PARAMETER_LIST:
+        content_based_model.train(params[0], params[1], params[2])
+        result = model_evaluator.evaluate(content_based_model, dh.interactions_val_indexed)
+        if (result['rate@5'] + result['rate@10']) > curr_max:
+            curr_max = result['rate@5'] + result['rate@10']
+            best_params = params
 
-    # content_based_model.train(best_params[0], best_params[1], best_params[2])
-    content_based_model.train(0, 0.5, (1, 3))
+    content_based_model.train(best_params[0], best_params[1], best_params[2])
 
     print("\n---CONTENT BASED MODEL---")
     print(model_evaluator.evaluate(content_based_model, dh.interactions_test_indexed))
