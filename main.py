194ed56298b0af02f9d6718ed8cbb665521e823b
mwiszenko <wiszenkom@gmail.com>
2021-06-03 01:09:57 +0200

    Optimise imports

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from flask import Flask
 from flask_restful import Api
 from service.recommendations import Recommendations
 
-from recommender.models import PopularityBasedRecommender, ContentBasedRecommender
-from recommender.data_handler import DataHandler
-
 app = Flask(__name__)
 api = Api(app)
 
